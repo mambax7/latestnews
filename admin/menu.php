8a860a084ed1b7c4d63a2564d1c3c0dad5ccd507
mambax7 <mambax7@gmail.com>
2017-09-13 04:07:01 -0400

    menu array

--- admin/menu.php
+++ admin/menu.php
@@ -7,26 +7,32 @@ if (false !== ($moduleHelper = Xmf\Module\Helper::getHelper($moduleDirName))) {
     $moduleHelper = Xmf\Module\Helper::getHelper('system');
 }
 
-
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 //$pathModIcon32 = $moduleHelper->getModule()->getInfo('modicons32');
 
 $moduleHelper->loadLanguage('modinfo');
 
-$adminmenu              = [];
-$i                      = 0;
-'title' =>  _AM_MODULEADMIN_HOME,
-'link' =>  'admin/index.php',
-'icon' =>  $pathIcon32 . '/home.png',
-++$i;
-'title' =>  _MI_LATESTNEWS_ADMIN_MENU1,
-'link' =>  'admin/blocksadmin.php',
-'icon' =>  $pathIcon32 . '/block.png',
-++$i;
-'title' =>  _MI_LATESTNEWS_ADMIN_MENU2,
-'link' =>  'admin/permissions.php',
-'icon' =>  $pathIcon32 . '/permissions.png',
-++$i;
-'title' =>  _AM_MODULEADMIN_ABOUT,
-'link' =>  'admin/about.php',
-'icon' =>  $pathIcon32 . '/about.png',
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_HOME,
+    'link'  => 'admin/index.php',
+    'icon'  => $pathIcon32 . '/home.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_LATESTNEWS_ADMIN_MENU1,
+    'link'  => 'admin/blocksadmin.php',
+    'icon'  => $pathIcon32 . '/block.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_LATESTNEWS_ADMIN_MENU2,
+    'link'  => 'admin/permissions.php',
+    'icon'  => $pathIcon32 . '/permissions.png',
+];
+
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_ABOUT,
+    'link'  => 'admin/about.php',
+    'icon'  => $pathIcon32 . '/about.png',
+];
+
